cca2b9a9f45ac4c0aba911694f118bb91f17ea81
mich balkany <askmitchb@gmail.com>
2024-01-02 12:59:08 -0500

    Initial Commit

---
+++ FaceFacts/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  FaceFacts
+//
+//  Created by Mich balkany on 1/2/24.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
