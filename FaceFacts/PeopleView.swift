5cfd5d948b144d1a91b2e746eb4120ff594b8472
mich balkany <askmitchb@gmail.com>
2024-01-03 11:39:56 -0500

    search filter, and querying the search items

---
+++ FaceFacts/PeopleView.swift
@@ -0,0 +1,18 @@
+//
+//  PeopleView.swift
+//  FaceFacts
+//
+//  Created by Mich balkany on 1/3/24.
+//
+
+import SwiftUI
+
+struct PeopleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PeopleView()
+}
